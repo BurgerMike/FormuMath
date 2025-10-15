fb4ba631c2b619ecc8d7791a528e3184d972453e
BurgerMike <mikelizondojr@hotmail.com>
2025-10-15 17:00:27 -0600

    Comit

--- Package.swift
+++ Package.swift
@@ -1,33 +1,17 @@
 // swift-tools-version: 6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "FormuMath",
     platforms: [
-        .macOS(.v14),
-        .iOS(.v16),
-        .tvOS(.v16),
-        .watchOS(.v9),
-        .visionOS(.v1)  
+        .macOS(.v14), .iOS(.v16), .tvOS(.v16), .watchOS(.v9), .visionOS(.v1)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "FormuMath",
-            targets: ["FormuMath"]
-        ),
+        .library(name: "FormuMath", targets: ["FormuMath"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "FormuMath"
-        ),
-        .testTarget(
-            name: "FormuMathTests",
-            dependencies: ["FormuMath"]
-        ),
+        .target(name: "FormuMath"),
+        .testTarget(name: "FormuMathTests", dependencies: ["FormuMath"]),
     ]
 )
+
