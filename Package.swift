a9d76518eff392fa915bb45bc9168df6c620ff19
BurgerMike <mikelizondojr@hotmail.com>
2025-10-15 16:36:43 -0600

    Cambio de version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "FormuMath",
     platforms: [
-        .macOS(.v13),
+        .macOS(.v14),
         .iOS(.v16),
         .tvOS(.v16),
         .watchOS(.v9),
